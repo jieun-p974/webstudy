1a8d1de2bf5655fa3f5af1c4b2f8dd58b775c3ae
jieun <jieun.p974@gmail.com>
2021-11-12 23:36:47 +0900

    js code modify

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ var app = http.createServer(function(request,response){
       return;
     }
     response.writeHead(200);
-    console.log(__dirname + url);
     response.end(fs.readFileSync(__dirname + url));
 
 });
