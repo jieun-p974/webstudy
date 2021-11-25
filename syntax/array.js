457634cd0e72b5f25d1678601a07eaf5464f35e6
jieun <jieun.p974@gmail.com>
2021-11-25 23:59:46 +0900

    반복문

---
+++ syntax/array.js
@@ -0,0 +1,12 @@
+//CRUD(Create, Read, Update, Delete)
+// 배열 생성
+var arr = ['A','B','C','D'];
+// 배열 읽기(배열 자릿수는 0부터 시작)
+console.log(arr[1]);
+console.log(arr[3]);
+// 배열 수정
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
