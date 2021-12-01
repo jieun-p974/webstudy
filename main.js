5482bf3548d42e71cfb0fcdfa5b6c2b0ac4a2c12
jieun <jieun.p974@gmail.com>
2021-12-01 21:13:06 +0900

    ui제작

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var url = require('url');
 
 function templateHTML(title, list, body){
-  return `<!doctype html>
+  return `
+  <!doctype html>
   <html>
   <head>
     <title>WEB1 - ${title}</title>
@@ -12,12 +13,12 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
-  `
+  `;
 }
-
 function templateList(filelist){
   var list = '<ul>';
   var i = 0;
@@ -35,29 +36,49 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
-        })
+        });
       } else {
         fs.readdir('./data', function(error, filelist){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
           });
         });
       }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
+
+
+
 });
 app.listen(3000);
