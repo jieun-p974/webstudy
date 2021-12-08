019338b2010c36f0cc5767d55561fe5f59485200
jieun <jieun.p974@gmail.com>
2021-12-08 20:28:19 +0900

    수정기능, 삭제기능 완성

--- main.js
+++ main.js
@@ -55,7 +55,10 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a> <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>`
             );
             response.writeHead(200);
             response.end(template);
@@ -102,11 +105,9 @@ var app = http.createServer(function(request,response){
           var template = templateHTML(title, list,
             `
             <form action="/update_process" method="post">
-            // 기존에 저장된 title 불러옴
               <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
               <p>
-              // 기존에 저장된 내용 불러옴
                 <textarea name="description" placeholder="description">${description}</textarea>
               </p>
               <p>
@@ -120,7 +121,39 @@ var app = http.createServer(function(request,response){
           response.end(template);
         });
       });
-    } else {
+    } else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          });
+      });
+    }else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
+      });
+    }else{
       response.writeHead(404);
       response.end('Not found');
     }
